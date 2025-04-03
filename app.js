9f3639dc3a87f26a452da344d9dff18085ae3e86
bemac01 <nwankwochidubem01@gmail.com>
2025-04-03 19:03:40 +0100

    array update

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 //Javascript codes 
 
-let text = "ASAP fortend";
+// let text = "ASAP fortend";
 
-for (let i = 0; i <= text.length; i++) {
-    if (i[8] - 1 == true){
-        console.log(text[i]);
-    }
-}
+// for (let i = 0; i <= text.length; i++) {
+//     if (i[8] - 1 == true){
+//         console.log(text[i]);
+//     }
+// }
+
+// filtering ages with array 
+let age = [16,17,14,15,19];
+
+let filteredArray = age.filter(element => {
+    return element >= 18
+})
+
+console.log(filteredArray)
