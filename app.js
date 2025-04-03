8128c1f0c1a197f6df51f49c94ee67f2cd191607
bemac01 <nwankwochidubem01@gmail.com>
2025-04-03 19:28:56 +0100

    array update

--- app.js
+++ app.js
@@ -9,10 +9,30 @@
 // }
 
 // filtering ages with array 
-let age = [16,17,14,15,19];
+// let age = [16,17,14,15,19];
 
-let filteredArray = age.filter(element => {
-    return element >= 18
-})
+// let filteredArray = age.filter(element => {
+//     return element >= 18
+// })
 
-console.log(filteredArray)
+// console.log(filteredArray)
+
+// write filtering in another traditional way 
+
+// create array of ages 
+let ages = [19,21,20,18,12]
+
+//adult array 
+let adults = []
+
+for (let i = 0; i < ages.length; i++) {
+    console.log(ages[i])
+    //check ages above 18 or 18 years old 
+    if ( ages[i] >=18 ) {
+        adults.push(ages[i])
+    }
+    //add filtered array to new array
+}
+
+//output new array filtered 
+console.log(adults)
